9a87200019abf40da5c06594921270b85949da25
AtEchoOff <midnightmarth@gmail.com>
2019-02-07 18:57:37 -0600

    Reducer now works as intended

--- client/taker/store/reducers/changeInput.js
+++ client/taker/store/reducers/changeInput.js
@@ -1,27 +1,39 @@
 const changeInput = (state = [], action) => {
   let newState = [...state];
   console.log("newState when state is coppied", newState);
+  let questionLocation;
+  if (newState.length > 0) {
+    for (let i in newState) {
+      if (newState[i].question == action.question) {
+        questionLocation = i;
+      }
+    }
+    console.log("didnt find question in state");
+  }
+
   switch (action.type) {
     case "EDIT_RANGE":
-      if (newState.length > 0) {
-        for (let i in newState) {
-          if (newState[i].question == action.question) {
-            newState[i].answer = action.answer;
-            return newState;
-          }
-        }
-        console.log("didnt find question in state");
+      if (questionLocation) {
+        newState[questionLocation].answer = action.answer;
+      } else {
+        newState.push({
+          question: action.question,
+          answer: action.answer
+        });
       }
-      newState.push({
-        question: action.question,
-        answer: action.answer
-      });
       console.log("Pushed new info to state: ", newState);
       return newState;
     case "EDIT_MULTI":
       console.log("editing Multi Question!");
-      return [...state, { question: action.question, answer: action.answer }];
-
+      if (questionLocation) {
+        newState[questionLocation].answer = action.answer;
+      } else {
+        newState.push({
+          question: action.question,
+          answer: action.answer
+        });
+      }
+      return newState;
     // case "EDIT_ORDER":
     //   console.log("editing Order Question!");
     //   return Object.assign(
