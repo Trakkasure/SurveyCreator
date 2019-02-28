52faa42fc65f227142ca8c1e524776c524a4fa4e
AtEchoOff <midnightmarth@gmail.com>
2019-02-28 01:19:00 -0600

    removed uneeded imports

--- client/creater/index.js
+++ client/creater/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
-
+import { Grid } from "@material-ui/core";
 import QuestionHolder from "./components/QuestionHolder.js";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  }
+});
 
 const handleSubmit = answers => {
   console.log("Submit button clicked!");
@@ -10,12 +16,12 @@ const handleSubmit = answers => {
 
 const App = () => {
   return (
-    <div>
+    <Grid>
       <QuestionHolder />
       <Button variant="contained" onClick={handleSubmit}>
         Submit
       </Button>
-    </div>
+    </Grid>
   );
 };
 
