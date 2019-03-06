1f4df4a742857202e05192b643eb866377e91958
AtEchoOff <midnightmarth@gmail.com>
2019-03-06 01:24:33 -0600

    Added FiniteValue question type. Fixed no text show bug for selects.

--- client/creater/components/Question.js
+++ client/creater/components/Question.js
@@ -8,68 +8,65 @@ import {
   Select
 } from "@material-ui/core";
 
-import { TrueFalse, OrderOfPrecidence, MultiChoice } from "./questionTypes/";
+import {
+  TrueFalse,
+  OrderOfPrecidence,
+  MultiChoice,
+  FiniteValue
+} from "./questionTypes/";
 
 const Question = props => {
-  const [qType, changeType] = useState("");
-  const [open, setOpen] = useState(false);
+  const [state, changeState] = useState("");
 
   return (
     <div key={props.key}>
       <TextField label="Question" />
 
       <div>
-        <form autoComplete="off">
-          <FormControl>
-            <InputLabel htmlFor="open-select">Question Type</InputLabel>
-            <Select
-              open={open}
-              onOpen={() => {
-                setOpen(true);
-              }}
-              onClose={() => {
-                setOpen(false);
-              }}
-              value={qType}
-              inputProps={{
-                name: "QuestionType",
-                id: "open-select"
-              }}
-              onChange={(e, value) => {
-                switch (value.props.value) {
-                  case 1:
-                    changeType("MultipleChoice");
-                    break;
-                  case 2:
-                    changeType("TrueFalse");
-                    break;
-                  case 3:
-                    changeType("OrderOfPrecidence");
-                    break;
-                  default:
-                    changeType("");
-                    break;
-                }
-              }}
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={1}>Multiple Choice</MenuItem>
-              <MenuItem value={2}>True/False</MenuItem>
-              <MenuItem value={3}>Order of Precidence</MenuItem>
-            </Select>
-            {qType === "MultipleChoice" ? (
-              <MultiChoice />
-            ) : qType === "TrueFalse" ? (
-              <TrueFalse />
-            ) : qType === "OrderOfPrecidence" ? (
-              <OrderOfPrecidence />
-            ) : (
-              <div>Choose Question Type</div>
-            )}
-          </FormControl>
-        </form>
+        <FormControl>
+          <InputLabel>Question Type</InputLabel>
+          <Select
+            value={state}
+            onChange={(e, value) => {
+              switch (value.props.value) {
+                case 1:
+                  changeState("1");
+                  break;
+                case 2:
+                  changeState("2");
+                  break;
+                case 3:
+                  changeState("3");
+                  break;
+                case 4:
+                  changeState("4");
+                  break;
+                default:
+                  changeState("");
+                  break;
+              }
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={1}>Multiple Choice</MenuItem>
+            <MenuItem value={2}>True/False</MenuItem>
+            <MenuItem value={3}>Order of Precidence</MenuItem>
+            <MenuItem value={4}>Finite Value</MenuItem>
+          </Select>
+          {state === "1" ? (
+            <MultiChoice />
+          ) : state === "2" ? (
+            <TrueFalse />
+          ) : state === "3" ? (
+            <OrderOfPrecidence />
+          ) : state === "4" ? (
+            <FiniteValue />
+          ) : (
+            <div>Choose Question Type</div>
+          )}
+        </FormControl>
       </div>
       <div>
         <Button
--- client/creater/components/questionTypes/index.js
+++ client/creater/components/questionTypes/index.js
@@ -1,5 +1,6 @@
 import MultiChoice from "./MultiChoice";
 import OrderOfPrecidence from "./OrderOfPrecidence";
 import TrueFalse from "./TrueFalse";
+import FiniteValue from "./FiniteValue";
 
-export { MultiChoice, OrderOfPrecidence, TrueFalse };
+export { MultiChoice, OrderOfPrecidence, TrueFalse, FiniteValue };
